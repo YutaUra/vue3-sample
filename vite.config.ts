e86a2b9925c5e52dc4a3d10145e57814b7b1aa7b
YutaUra <yuuta3594@outlook.jp>
2022-04-14 08:37:28 +0900

    analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      plugins: [visualizer()],
+    },
+  },
 })
